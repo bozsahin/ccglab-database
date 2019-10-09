fec0a770ba434e1195acbe73eece1ef140ffe6e9
bozsahin <cem.bozsahin@gmail.com>
2019-10-09 18:23:14 +0300

    added nuosu, japanese and shipibo

--- grammars/cl-db1/g-test.lisp
+++ grammars/cl-db1/g-test.lisp
@@ -23,6 +23,8 @@
 (p-aux '("this book" i like) '(NP) 'S)     ; parses the type-raised grammar
 (p-aux '(i picked the book up) '(NP UP) 'S)
 (p-aux '(i picked up the book) '(NP UP) 'S)
+(p-aux '(the beans that you spilled caused "quite a stir") '(NP) 'S)
+(p-aux '(she thinks harry and barry likes mary) '(NP) 'S)
 
 
 ;; Mandarin Chinese
@@ -36,6 +38,32 @@
 (p-aux '(zhangsan wo qidai ta "yao lai") '(NP) 'S)
 (p-aux '(zhangsan wo quan ta "yao lai")' '(NP) 'S)
 (p-aux '(zhangsan wo daying ta "yao lai") '(NP) 'S)
+
+;; Japanese
+
+(lg "g" :make t)            ; makes g.ded from g.ccg 
+(tr "g.ded" '(jp))         ; Japanese TR
+(savetr "g-jp.ded")        ; saves grammar with TR rules
+(lg "g-jp")                ; lg assumes .ded suffix
+(p-aux '(taroo-wa hanako-ni kiteiru huku-o yogos-are-ta) '(NP PredP) 'S) 
+
+;; Shipibo
+
+(lg "g" :make t)            ; makes g.ded from g.ccg 
+(tr "g.ded" '(sh))         ; Shipibo TR
+(savetr "g-sh.ded")        ; saves grammar with TR rules
+(lg "g-sh")                ; lg assumes .ded suffix
+(p-aux '(ochiti-baon-ra bake natex-kan-ke) '(NP) 'S) 
+(p-aux '(joni-bo-ra mawa-kan-ke) '(NP) 'S) 
+(p-aux '(ochiti-nin-ra bake-bo natex-kan-ke) '(NP) 'S) 
+
+;; Liangshan Nuosu
+
+(lg "g" :make t)            ; makes g.ded from g.ccg 
+(tr "g.ded" '(nu))         ; Nousu TR
+(savetr "g-nu.ded")        ; saves grammar with TR rules
+(lg "g-nu")                ; lg assumes .ded suffix
+(p-aux '(m33ka55 m33ko44 ndu21) '(NP PredP) 'S) 
 
 ;; Turkish
 
@@ -69,6 +97,9 @@
 (lg "g-dy")                ; lg assumes .ded suffix
 (p-aux '("bayi yara" numa-ngu giga-n gubi-ngu mawa-li) '(NP VP) 'S)
 (p-aux '("bayi yara" numa-ngu giga-n banaga-nyu) '(NP VP) 'S)
+(type-raise 'off)
+(p '(bagul wanal-gu banul-din-gu yara-nu-n-din-gu))  ; use p---cannot retrieve only results onto high types
+(ders)
 
 ;; German
 
